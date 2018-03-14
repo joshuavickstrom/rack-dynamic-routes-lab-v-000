892cb26d0a678890f342f34f66ef16f3f8b63094
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-14 23:16:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,12 +6,5 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/<ITEM NAME>"
-      return @@item['price']
-    else
-      resp.write "Item not found"
-      resp.status = 404
-    end
-  end
-  resp.finish
+
 end
