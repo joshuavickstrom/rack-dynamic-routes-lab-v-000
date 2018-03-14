f9df3378d948a8f59620bfecce002be73e94db85
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-14 23:15:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack:Request.new(env)
+    req = Rack::Request.new(env)
 
     if req.path=="/items/<ITEM NAME>"
       return @@item['price']
