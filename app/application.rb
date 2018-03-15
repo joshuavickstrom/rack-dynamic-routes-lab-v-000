57693565c9009cb72005c0f6c7cddb8924ca98b5
Joshua Vickstrom <joshuavickstrom@outlook.com>
2018-03-15 00:07:58 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,9 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split('/items/').last
-      if @@items.find{ |i| i.name == item}
+      if found_item = @@items.find{ |i| i.name == item}
         binding.pry
-          resp.write "#{item.price}"
+          resp.write "#{found_item.price}"
           resp.status = 200
       else
         resp.write "Item not found"
